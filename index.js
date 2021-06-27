0c5234178cc5c0619b950e63544b8d987003a2a5
nid lilly <46010125+nidlilly@users.noreply.github.com>
2021-06-27 22:35:01 +0000

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 for (let i = 1; i < 101; i++) {
-  //let inputNum = parseInt(prompt("Enter a number: "));
-  if (i % 15 == 0)
+  if (i % 15 == 0) //if divisible by 5 and 3
    alert(i + ", FizzBuzz");
-  else if (i % 5 == 0)
+  else if (i % 5 == 0) //if divisible by 5
     alert(i + ", Buzz")
-  else if (i % 3 == 0)
+  else if (i % 3 == 0) //if divisible by 3
     alert(i + ", Fizz")
-  else 
+  else //not divisible by 5 or 3
     alert(i)
 }
